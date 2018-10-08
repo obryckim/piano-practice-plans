c02ab07f2cbb7ad4b83cb4770647eaa4077b2104
Michael Obrycki <obryckim@users.noreply.github.com>
2018-10-08 07:32:12 -0400

    controller updates

--- src/PracticePlans.WebApi/Controllers/PracticePlansController.cs
+++ src/PracticePlans.WebApi/Controllers/PracticePlansController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -7,8 +8,9 @@ using PracticePlans.Common.Azure.Storage.Repositories;
 
 namespace PracticePlans.WebApi.Controllers
 {
-    [Route("api/[controller]")]
     [ApiController]
+    [Route("api/[controller]")]
+    [Produces("application/json")]
     public class PracticePlansController : ControllerBase
     {
         private readonly IPracticePlanRepository practicePlanRepository;
@@ -22,7 +24,23 @@ namespace PracticePlans.WebApi.Controllers
         public async Task<ActionResult<IEnumerable<IPracticePlan>>> GetAsync()
         {
             var practicePlans = await this.practicePlanRepository.GetAllAsync();
-            return practicePlans.ToList();
+            return this.Ok(practicePlans);
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(404)]
+        [Route("{startDate:DateTime}")]
+        public async Task<ActionResult<IPracticePlan>> Get(DateTime startDate)
+        {
+            var practicePlan = await this.practicePlanRepository.GetAsync(startDate);
+
+            if (practicePlan == null)
+            {
+                return this.NotFound();
+            }
+
+            return this.Ok(practicePlan);
         }
     }
 }
