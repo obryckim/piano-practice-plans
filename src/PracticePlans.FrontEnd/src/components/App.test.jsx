8382316352457c2b2af1e9e6e6c516ca436ed99e
Michael Obrycki <obryckim@users.noreply.github.com>
2018-10-14 08:41:43 -0400

    setup testing framework; closes #20

---
+++ src/PracticePlans.FrontEnd/src/components/App.test.jsx
@@ -0,0 +1,24 @@
+﻿import React from 'react';
+import expect from 'expect';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import App from './App';
+
+Enzyme.configure({
+    adapter: new Adapter()
+});
+
+function setup() {
+    const props = {};
+    return shallow(<App {...props} />);
+}
+
+describe('<App />', () => {
+    it('renders h1', () => {
+        const wrapper = setup();
+        var h1 = wrapper.find('h1');
+        expect(h1.length).toBe(1);
+        expect(h1.text()).toEqual('This is the app...');
+    });
+});
+
