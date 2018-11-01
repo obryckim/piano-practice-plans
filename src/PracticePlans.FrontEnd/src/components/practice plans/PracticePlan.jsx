5179bd41ff47da7e2da5cd390cdde2add711854f
Michael Obrycki <obryckim@users.noreply.github.com>
2018-11-01 17:40:56 -0400

    created default page for viewing most recent practice plan; closes #37

--- src/PracticePlans.FrontEnd/src/components/practice plans/PracticePlan.jsx
+++ src/PracticePlans.FrontEnd/src/components/practice plans/PracticePlan.jsx
@@ -1,11 +1,18 @@
 ﻿import React from 'react';
 import PropTypes from 'prop-types';
+import moment from 'moment';
 
 const PracticePlan = ({ practicePlan }) => {
     return (
         <div>
             <h1>Benjamin&apos;s Piano Practice Plan</h1>
-            <h3><small className='text-muted'>{practicePlan.startDateString} - {practicePlan.startDateString}</small></h3>
+            <h3>
+                <small className='text-muted'>
+                    {moment(practicePlan.startDate).format('ddd, MMM D')}
+                    &nbsp;through&nbsp;
+                    {moment(practicePlan.startDate).add(6, 'days').format('ddd, MMM D')}
+                </small>
+            </h3>
             <div className='card'>
                 <div className='card-body' dangerouslySetInnerHTML={practicePlan}></div>
             </div>
