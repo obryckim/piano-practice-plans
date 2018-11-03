13613e47c90c3e16b38134d8cbf6c97cc6e5d7e9
Michael Obrycki <obryckim@users.noreply.github.com>
2018-11-03 15:36:20 -0400

    added dotenv-webpack

--- src/PracticePlans.FrontEnd/src/api/apiCommon.js
+++ src/PracticePlans.FrontEnd/src/api/apiCommon.js
@@ -1,2 +1,2 @@
-﻿export const BASE_URL = process.env.API_URL || 'http://localhost:5000';
+﻿export const BASE_URL = process.env.API_URL;
 export const PRACTICE_PLANS_ENDPOINT = BASE_URL + '/api/PracticePlans/';
